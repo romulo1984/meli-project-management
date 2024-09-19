b3ea4066aa433d12d4650b1b450dd0e0b74a81ae
Rômulo Guimarães <romulo1984@gmail.com>
2024-09-19 14:23:33 -0300

    Added context menu in note cards

--- src/components/note/card.tsx
+++ src/components/note/card.tsx
@@ -1,28 +1,30 @@
-"use client";
-import useRetro from "@/helpers/hooks/useRetro";
-import RandomNames from "@/helpers/randomNames";
-import SpechText from "@/helpers/spechText";
-import { api } from "@convex/_generated/api";
-import { Doc, Id } from "@convex/_generated/dataModel";
-import { useMutation } from "convex/react";
-import Image from "next/image";
-import { useMemo, useState } from "react";
-import DropdownSelect from "../dropdownSelect";
-import { AnonymousIcon, DeleteIcon, LikeIcon, SpeakerIcon } from "../icons";
-import NoteBody from "../note-body";
-import NoteForm from "../note-form";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
-import { toast } from "react-toastify";
+'use client'
+import useRetro from '@/helpers/hooks/useRetro'
+import RandomNames from '@/helpers/randomNames'
+import SpechText from '@/helpers/spechText'
+import { api } from '@convex/_generated/api'
+import { Doc, Id } from '@convex/_generated/dataModel'
+import { useMutation } from 'convex/react'
+import Image from 'next/image'
+import { useMemo, useState, useRef, useCallback } from 'react'
+import DropdownSelect from '../dropdownSelect'
+import { AnonymousIcon, LikeIcon } from '../icons'
+import NoteBody from '../note-body'
+import NoteForm from '../note-form'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
+import { ContextMenu } from 'primereact/contextmenu'
+import { MenuItem } from 'primereact/menuitem'
+import { ConfirmPopup } from 'primereact/confirmpopup'
 
 interface NoteProps {
-  note: Doc<"notes">
-  user: Doc<"users"> | undefined | null
-  me?: Doc<"users"> | undefined | null
+  note: Doc<'notes'>
+  user: Doc<'users'> | undefined | null
+  me?: Doc<'users'> | undefined | null
   actionType?: boolean
   blur?: boolean
   highlighted?: boolean
-  childrenNotes?: Doc<"notes">[]
+  childrenNotes?: Doc<'notes'>[]
   roundTop: boolean
   roundBottom: boolean
 }
@@ -33,44 +35,50 @@ interface NoteStructure {
 }
 
 export default function NoteCard(props: NoteProps) {
-  const { note, user, me, actionType, blur = false, roundTop, roundBottom } = props;
-  const { users } = useRetro({ retroId: note.retroId });
-  const [speaking, setSpeaking] = useState(false);
+  const {
+    note,
+    user,
+    me,
+    actionType,
+    blur = false,
+    roundTop,
+    roundBottom,
+  } = props
+  const { users } = useRetro({ retroId: note.retroId })
   const [editing, setEditing] = useState({
     value: false,
     note: {
       body: note.body,
-      anonymous: Boolean(note.anonymous)
-    }
+      anonymous: Boolean(note.anonymous),
+    },
   })
   const [deleteIntention, setDeleteIntention] = useState(false)
-  const AssigneNote = useMutation(api.notes.assigne);
-  const UnnasignNote = useMutation(api.notes.unnasign);
-  const UpdateNote = useMutation(api.notes.update);
-  const RemoveNote = useMutation(api.notes.remove);
-  const LikeNote = useMutation(api.notes.likeToggle);
+  const cardRef = useRef<HTMLDivElement>(null)
+  const cardContextMenuRef = useRef<ContextMenu>(null)
+  const AssigneNote = useMutation(api.notes.assigne)
+  const UnnasignNote = useMutation(api.notes.unnasign)
+  const UpdateNote = useMutation(api.notes.update)
+  const RemoveNote = useMutation(api.notes.remove)
+  const LikeNote = useMutation(api.notes.likeToggle)
 
-  const isOwner = me?._id === user?._id;
+  const isOwner = me?._id === user?._id
 
-  const isAnonymous = note.anonymous !== undefined && note.anonymous === true;
+  const isAnonymous = note.anonymous !== undefined && note.anonymous === true
   const obfuscate = blur && !isOwner
 
-  const randomName = useMemo(() => RandomNames(), []);
+  const randomName = useMemo(() => RandomNames(), [])
 
-  const speechNote = () => {
-    setSpeaking(true);
-    SpechText(note?.body, "native").finally(() => {
-      setSpeaking(false);
-    });
-  };
+  const speechNote = useCallback(() => {
+    SpechText(note?.body, 'native')
+  }, [note])
 
-  const assigneHandler = (userId: Id<"users">) => {
-    AssigneNote({ noteId: note._id, userId: userId });
-  };
+  const assigneHandler = (userId: Id<'users'>) => {
+    AssigneNote({ noteId: note._id, userId: userId })
+  }
 
   const unnasignHandler = () => {
-    UnnasignNote({ noteId: note._id });
-  };
+    UnnasignNote({ noteId: note._id })
+  }
 
   const editionHandler = (data: NoteStructure) => {
     if (!isOwner) return
@@ -81,10 +89,10 @@ export default function NoteCard(props: NoteProps) {
     })
   }
 
-  const assignedTo = users?.find((u) => u?._id === note.assignedTo);
+  const assignedTo = users?.find(u => u?._id === note.assignedTo)
 
   const youLiked =
-    me && note.likes && note.likes.length > 0 && note.likes.includes(me._id);
+    me && note.likes && note.likes.length > 0 && note.likes.includes(me._id)
 
   const LeftBottomIcons = () => {
     if (actionType)
@@ -95,7 +103,7 @@ export default function NoteCard(props: NoteProps) {
           assigneHandler={assigneHandler}
           unnasignHandler={unnasignHandler}
         />
-      );
+      )
 
     if (obfuscate) {
       return (
@@ -103,7 +111,7 @@ export default function NoteCard(props: NoteProps) {
           <AnonymousIcon />
           <span className="text-zinc-400 text-xs">Hidden</span>
         </div>
-      );
+      )
     }
 
     if (isAnonymous)
@@ -111,29 +119,28 @@ export default function NoteCard(props: NoteProps) {
         <div>
           <AnonymousIcon />
           <span className="text-zinc-400 text-xs">
-            {isOwner ? "You" : randomName}
+            {isOwner ? 'You' : randomName}
           </span>
         </div>
-      );
+      )
 
     return (
       <div>
         <Image
-          alt={user?.name || ""}
+          alt={user?.name || ''}
           className="w-5 h-5 rounded-full inline-block mr-2"
-          src={user?.avatar || ""}
+          src={user?.avatar || ''}
           width={24}
           height={24}
         />
         <span className="text-zinc-400 text-xs">
-          {isOwner ? "You" : user?.name}
+          {isOwner ? 'You' : user?.name}
         </span>
       </div>
-    );
-  };
+    )
+  }
 
-
-  const toggleEdition = () => {
+  const toggleEdition = useCallback(() => {
     if (!isOwner) return
 
     setEditing({
@@ -141,34 +148,16 @@ export default function NoteCard(props: NoteProps) {
       note: {
         body: note.body,
         anonymous: Boolean(note.anonymous),
-      }
+      },
     })
-  }
+  }, [isOwner, note])
 
-  const removeHandler = () => {
+  const removeHandler = useCallback(() => {
     RemoveNote({ id: note._id })
-  }
+  }, [RemoveNote, note._id])
 
   const likeHandler = () => {
     LikeNote({ noteId: note._id, userId: me!._id })
-  }
-
-  const handleIntentionalDelete = () => {
-    if (!isOwner) return
-
-    if (deleteIntention) {
-      removeHandler();
-      setDeleteIntention(false)
-
-      return
-    }
-
-    const delay = 4000
-    toast.error('Please, click one more time to confirm the delete action', {
-      autoClose: delay
-    })
-    setTimeout(setDeleteIntention, delay, false)
-    setDeleteIntention(true)
   }
 
   const containerStyle = useMemo<string>(() => {
@@ -180,26 +169,62 @@ export default function NoteCard(props: NoteProps) {
     return styles.join(' ')
   }, [roundTop, roundBottom])
 
+  const showContextMenu = (e: React.MouseEvent) => {
+    e.preventDefault()
+    cardContextMenuRef?.current?.show(e)
+  }
+
+  const contextMenuItems = useMemo<MenuItem[]>(
+    () => [
+      {
+        label: 'Speech',
+        icon: 'pi pi-volume-up',
+        command: speechNote,
+      },
+      {
+        label: 'Edit',
+        icon: 'pi pi-pen-to-square',
+        disabled: !isOwner,
+        command: toggleEdition,
+      },
+      {
+        label: 'Delete',
+        icon: 'pi pi-trash',
+        disabled: !isOwner,
+        command: () => setDeleteIntention(true),
+      },
+    ],
+    [isOwner, speechNote, toggleEdition],
+  )
+
   return (
     <div
+      ref={cardRef}
       title={note.body}
       className={`transition-all w-full bg-white p-3 text-zinc-500 text-sm shadow ${containerStyle}`}
       onDoubleClick={toggleEdition}
+      onContextMenu={showContextMenu}
     >
-      <div className={`mb-2 ${obfuscate ? 'blur-sm' : ''}`} >
-        {!editing.value && <NoteBody note={note} users={users} obfuscate={obfuscate} />}
+      <div className={`mb-2 ${obfuscate ? 'blur-sm' : ''}`}>
+        {!editing.value && (
+          <NoteBody note={note} users={users} obfuscate={obfuscate} />
+        )}
         {editing.value && (
           <NoteForm
             opened={editing.value}
-            toggleOpened={() => setEditing(old => ({
-              ...old,
-              value: false,
-            }))}
+            toggleOpened={() =>
+              setEditing(old => ({
+                ...old,
+                value: false,
+              }))
+            }
             newNote={editing.note}
-            setNewNote={(newNote) => setEditing(old => ({
-              ...old,
-              note:  newNote
-            }))}
+            setNewNote={newNote =>
+              setEditing(old => ({
+                ...old,
+                note: newNote,
+              }))
+            }
             saveHandler={(_: React.FormEvent<HTMLFormElement>) => {
               editionHandler(editing.note)
               setEditing(old => ({ ...old, value: false }))
@@ -212,12 +237,7 @@ export default function NoteCard(props: NoteProps) {
         {LeftBottomIcons()}
 
         {!obfuscate && (
-          <div className="flex justify-end items-center gap-3">
-            {isOwner && (
-              <div onClick={toggleEdition} className="text-zinc-400">
-                <FontAwesomeIcon icon={faEdit} />
-              </div>
-            )}
+          <div className="flex justify-end items-center gap-2">
             <div
               onClick={likeHandler}
               className="flex items-center justify-center gap-1"
@@ -227,17 +247,23 @@ export default function NoteCard(props: NoteProps) {
                 <p className="text-xs text-zinc-400">{note.likes.length}</p>
               )}
             </div>
-            <div onClick={speechNote}>
-              <SpeakerIcon speaking={speaking} />
-            </div>
-            {isOwner && (
-              <div onClick={handleIntentionalDelete} title="Click twice to delete this card">
-                <DeleteIcon fill={deleteIntention ? 'red-500' : 'zinc-400'} />
-              </div>
-            )}
+            <FontAwesomeIcon
+              onClick={showContextMenu}
+              className="px-2"
+              icon={faEllipsisVertical}
+            />
           </div>
         )}
       </div>
+      <ContextMenu model={contextMenuItems} ref={cardContextMenuRef} />
+      <ConfirmPopup
+        target={cardRef.current || undefined}
+        visible={deleteIntention}
+        onHide={() => setDeleteIntention(false)}
+        message="Are you sure you want to proceed?"
+        icon="pi pi-exclamation-triangle"
+        accept={removeHandler}
+      />
     </div>
-  );
+  )
 }
